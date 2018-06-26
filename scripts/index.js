024e4d2fe1d476b13fe93fb5a613b934e43c431f
agnekil <nathalie@agnekil.com>
2018-06-26 14:09:36 +0200

    hämtat 10 senaste annonserna

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,19 @@
-async function searchByCriteria(searchCriteria) {
-  const baseURL = 'http://api.arbetsformedlingen.se/af/v0/';
-  const responseObject = await fetch(baseURL + searchCriteria);
-  const matches = await responseObject.json();
-  console.log(matches);
-}
+// async function searchByCriteria(searchCriteria) {
+//   const baseURL = 'http://api.arbetsformedlingen.se/af/v0/';
+//   const responseObject = await fetch(baseURL + searchCriteria);
+//   const matches = await responseObject.json();
+//   console.log(matches);
+// }
 
-searchByCriteria('platsannonser/matchning?lanid=1&yrkesomradeid=3&antalrader=30');
+// searchByCriteria('platsannonser/matchning?lanid=1&yrkesomradeid=3&antalrader=30');
+
+fetch("http://api.arbetsformedlingen.se/af/v0/platsannonser/matchning?lanid=1")
+  .then(response => response.json())
+  .then(result => {
+    // console.log(result.matchningslista.matchningdata);
+    for (let i = 0; i < 10; i++) {
+      let element = result.matchningslista.matchningdata[i];
+      console.log(element);
+      
+    }
+  })
