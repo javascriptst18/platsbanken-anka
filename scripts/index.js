e7ddf1c9583289d6565244756064c83b630dd341
KajsaLindholm <kajsamlindholm@gmail.com>
2018-06-26 14:38:13 +0200

    choose number of jobs visible, 1st commit

--- scripts/index.js
+++ scripts/index.js
@@ -7,13 +7,25 @@
 
 // searchByCriteria('platsannonser/matchning?lanid=1&yrkesomradeid=3&antalrader=30');
 
+const valAntal = document.getElementById('valAntal');
+let nyttAntal = 10;
+
 fetch("http://api.arbetsformedlingen.se/af/v0/platsannonser/matchning?lanid=1")
   .then(response => response.json())
   .then(result => {
     // console.log(result.matchningslista.matchningdata);
-    for (let i = 0; i < 10; i++) {
+    for (let i = 0; i < nyttAntal; i++) {
       let element = result.matchningslista.matchningdata[i];
       console.log(element);
       
     }
   })
+
+function antalAnnonser(event) {
+  event.preventDefault();
+  const form = event.target;
+  nyttAntal = form.antal.value;
+  return nyttAntal;
+}
+
+valAntal.addEventListener('submit', antalAnnonser);
