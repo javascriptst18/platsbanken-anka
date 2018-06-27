7a21f48d806e97614423afa5ad64b02baeb74197
agnekil <nathalie@agnekil.com>
2018-06-27 12:03:07 +0200

    skapat funktion för att hämta annonser per yrkesområde och lagt ut det på sidan i knappar. Länkarna funkar inte än, men måste fixa CSS först det ser för taskigt ut..

--- scripts/index.js
+++ scripts/index.js
@@ -28,7 +28,7 @@ function getAdsAndPrint() {
 function getCardInfo(result) {
   for (let i = 0; i < searchVariables.nyttAntal; i++) {
     let element = result.matchningslista.matchningdata[i];
-    console.log(element);
+    // console.log(element);
 
     let lastApplyDate = result.matchningslista.matchningdata[i].sista_ansokningsdag;
     let applyDateSplit = lastApplyDate.split("", 10);
@@ -71,3 +71,27 @@ function handleSearch(event) {
 getAdsAndPrint();
 forms.valAntal.addEventListener('submit', antalAnnonser);
 forms.jobSearch.addEventListener('submit', handleSearch);
+
+
+
+
+// Hämtar annonser per yrkesområde
+function getAdsByField() {
+  let url = `http://api.arbetsformedlingen.se/af/v0/platsannonser/soklista/yrkesomraden`;
+  fetch(url)
+    .then(response => response.json())
+    .then(result => {
+      let fieldList = result.soklista.sokdata;
+      for (let i = 0; i < fieldList.length; i++) {
+        let element = fieldList[i];
+        console.log(element);
+    
+        let getLinks = document.querySelector("#fieldList");
+        let link = `<a href="${element.namn}"><button class="buttonField">${element.namn}</button></a>`;
+    
+        getLinks.insertAdjacentHTML("beforeend", link);
+      }
+      console.log(fieldList);
+    })
+}
+getAdsByField()
